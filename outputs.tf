a0b349b121b7bc2b6b501bbeaaa304e3eb6f0633
Alexander Mancevice <amancevice@cargometrics.com>
2018-06-07 12:00:00 -0400

    output name

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "request_url" {
   value       = "${google_cloudfunctions_function.function.https_trigger_url}"
 }
 
-output "topics" {
+output "pubsub_topics" {
   description = "Pub/Sub topics created."
   value       = "${google_pubsub_topic.topic.*.name}"
 }
